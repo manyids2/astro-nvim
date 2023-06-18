5812ee2474023f1d21d4f8ebba88bedf2132c09f
manyids2 <arun.mukundan@gmail.com>
2023-06-18 10:31:19 +0200

    heavy with all features

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  -- editing
+  { import = "astrocommunity.editing-support.cutlass-nvim" },
+
   -- languages
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
@@ -14,6 +17,16 @@ return {
   { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 
-  -- ide
+  -- appearance
+  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
+
+  -- ide
+  { import = "astrocommunity.code-runner.sniprun" },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
 }
