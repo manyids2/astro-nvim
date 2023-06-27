baf9fef41e6dbe01a0b97a2e52e3ed7dfbfea6b1
manyids2 <arun.mukundan@gmail.com>
2023-06-27 23:46:08 +0200

    subversive use m instead of c

--- plugins/vim_subversive.lua
+++ plugins/vim_subversive.lua
@@ -2,12 +2,12 @@ return {
   "https://github.com/svermeulen/vim-subversive",
   config = function()
     -- s is always used, so use c instead
-    vim.cmd [[nmap c <plug>(SubversiveSubstitute)]]
-    vim.cmd [[nmap cc <plug>(SubversiveSubstituteLine)]]
+    vim.cmd [[nmap m <plug>(SubversiveSubstitute)]]
+    vim.cmd [[nmap mm <plug>(SubversiveSubstituteLine)]]
 
-    vim.cmd [[nmap <leader>c <plug>(SubversiveSubstituteRange)]]
-    vim.cmd [[xmap <leader>c <plug>(SubversiveSubstituteRange)]]
-    vim.cmd [[nmap <leader>cc <plug>(SubversiveSubstituteWordRange)]]
+    vim.cmd [[nmap <leader>m <plug>(SubversiveSubstituteRange)]]
+    vim.cmd [[xmap <leader>m <plug>(SubversiveSubstituteRange)]]
+    vim.cmd [[nmap <leader>mm <plug>(SubversiveSubstituteWordRange)]]
     vim.cmd [[onoremap ie :exec "normal! ggVG"<cr>]]
     vim.cmd [[onoremap iv :exec "normal! HVL"<cr>]]
   end,
