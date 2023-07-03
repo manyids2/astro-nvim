8ac0daf513cea1621f00f18009ec0aea4125bf08
manyids2 <arun.mukundan@gmail.com>
2023-07-03 23:30:21 +0200

    Disable notify, add themes

---
+++ plugins/themes.lua
@@ -0,0 +1,12 @@
+return {
+  "rktjmp/lush.nvim",
+  "catppuccin/nvim",
+  "bluz71/vim-moonfly-colors",
+  "bluz71/vim-nightfly-colors",
+  "folke/tokyonight.nvim",
+  "aktersnurra/no-clown-fiesta.nvim",
+  "nyoom-engineering/oxocarbon.nvim",
+  "mcchrish/zenbones.nvim",
+  "nvim-tree/nvim-web-devicons",
+  "EdenEast/nightfox.nvim",
+}
